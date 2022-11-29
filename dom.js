d21eb25e3a15b7f649560d90489a4afd145448f9
handagenalini <handagenalini@gmail.com>
2022-11-29 23:01:08 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,19 @@
+console.dir(document)
+console.log(document.URL)
+console.log(document.domain)
+console.log(document.title)
+document.title=123
+console.log(document.title)
+console.log(document.doctype)
+console.log(document.head)
+console.log(document.body)
+console.log(document.all)
+console.log(document.images)
+console.log(document.all[10])
+console.log(document.links)
+var headerTitle=document.getElementById("header-title")
+headerTitle.textcontent="hello";
+// headerTitle.innerText="good"
+// console.log(headerTitle.innerText)
+// headerTitle.innerHTML='<h3>hello</h3>'
+
