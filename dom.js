cb41f6fe25bc88b18a67a0dc7f8f4e2fc83d3109
handagenalini <handagenalini@gmail.com>
2022-12-07 17:12:42 +0530

    first commit

--- dom.js
+++ dom.js
@@ -25,7 +25,8 @@ console.log(items)
 console.log(items[1])
 items[1].textContent="hello2"
 items[1].style.fontWeight="bold"
-items[1].style.backgroundColor="yellow"
+items[2].style.backgroundColor="green"
 for(var i=0;i<items.length;i++){
-    items[i].style.backgroundColor="red"
+    items[i].style.fontWeight="bold"
+    items[i].style.color="red"
 }
