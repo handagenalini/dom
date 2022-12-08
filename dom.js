c2efc0f20311f69ae9f35674f4815879a6c64829
handagenalini <handagenalini@gmail.com>
2022-12-08 17:45:52 +0530

    first commit

--- dom.js
+++ dom.js
@@ -30,13 +30,44 @@
 //     items[i].style.fontWeight="bold"
 //     items[i].style.color="red"
 // }
-var li=document.getElementsByClassName("list-group-item")
-console.log(li)
-console.log(li[1])
-li[1].textContent="hello2"
-li[1].style.fontWeight="bold"
-li[2].style.backgroundColor="green"
-for(var i=0;i<li.length;i++){
-    li[i].style.fontWeight="bold"
-    li[i].style.color="red"
+// var li=document.getElementsByTagName('li')
+// console.log(li)
+// console.log(li[1])
+// li[1].textContent="hello2"
+// li[1].style.fontWeight="bold"
+// li[2].style.backgroundColor="green"
+// for(var i=0;i<li.length;i++){
+//     li[i].style.fontWeight="bold"
+//     li[i].style.color="red"
+// }
+var header=document.querySelector("#main-header");
+header.style.borderBottom="solid 4px #ccc"
+
+var input=document.querySelector("input")
+input.value="hello world"
+var submit=document.querySelector("input[type='submit']")
+submit.value='send'
+
+var item=document.querySelector('.list-group-item')
+item.style.color='red'
+
+var lastitem=document.querySelector(".list-group-item:last-child")
+lastitem.style.color="blue"
+
+var seconditem=document.querySelector('.list-group-item:nth-child(2)')
+seconditem.style.color="coral"
+
+var titles=document.querySelectorAll('.title')
+console.log(titles)
+titles[0].textContent="hello"
+
+var odd=document.querySelectorAll('li:nth-child(odd)')
+var even=document.querySelectorAll('li:nth-child(even)')
+
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='#f4f4f4'
+    even[i].style.backgroundColor='coral'
+
 }
+
+
